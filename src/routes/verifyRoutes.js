8485cf4808eaa0aa68df64b72f02e53bc5e0a379
Pratik Agrawal <pratikben.1000@gmail.com>
2026-03-12 12:36:52 +0530

    remove face verify

--- src/routes/verifyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { verifyFace } from "../controllers/faceVerifyController.js";
-
-const router = express.Router();
-
-router.post("/", verifyFace);
-
-export default router;
